e5b6cbf440393945942f4aba085e7b46c332e52a
BTTBao <lebao.071205@gmail.com>
2024-10-15 09:04:01 +0700

    bigup

--- function/nhanVien.h
+++ function/nhanVien.h
@@ -10,13 +10,12 @@ int ShowStaffMenu(UserAccount user)
     int choice;
 
     cout << "==================== Menu Quan Ly Khach Hang ====================" << endl;
-    cout << "1. Them khach hang moi" << endl;
-    cout << "2. Xem danh sach khach hang" << endl;
-    cout << "3. Cap nhat thong tin khach hang" << endl;
-    cout << "4. Xoa khach hang" << endl;
-    cout << "5. Tim kiem khach hang" << endl;
-    cout << "6. Xem danh sach hoa don" << endl;
-    cout << "7. Thoat" << endl;
+    cout << "1. Xem danh sach khach hang" << endl;
+    cout << "2. Cap nhat thong tin khach hang" << endl;
+    cout << "3. Xoa khach hang" << endl;
+    cout << "4. Tim kiem khach hang" << endl;
+    cout << "5. Xem danh sach hoa don" << endl;
+    cout << "6. Thoat" << endl;
     cout << "Nhap lua chon cua ban: ";
     cin >> choice;
 
