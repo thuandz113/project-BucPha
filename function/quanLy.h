fbcf04324826d9497ae79d29259bc9cfba5258e3
BTTBao <lebao.071205@gmail.com>
2024-10-17 14:01:27 +0700

    update quanly3

--- function/quanLy.h
+++ function/quanLy.h
@@ -12,13 +12,14 @@ int ShowManageMenu(UserAccount user)
 {
     QuanLyChucNang qlcn;
     int choice;
-
+    do{
     cout << "==================== Menu Quan Ly ====================" << endl;
     cout << "1. Nhap hang" << endl;
     cout << "2. Xoa hang" << endl;
     cout << "3. Cap nhat danh sach nhan vien" << endl;
-    cout << "4. Xoa khach hang" << endl;
-    cout << "5. Thoat" << endl;
+    cout << "4. Giao dien nguoi dung"<<endl;
+    cout << "5. Giao dien nhan vien"<<endl;
+    cout << "6. Thoat" << endl;
     cout << "Nhap lua chon cua ban: ";
     cin >> choice;
 
@@ -32,16 +33,14 @@ int ShowManageMenu(UserAccount user)
         case 3:
             menuEmployeeManagement();
             break;
-        // case 4:
-        //     qlcn.xoaKhachHang();
-        //     break;
-        case 5:
+        case 4:
             cout << "Thoat khoi chuong trinh." << endl;
             break;
         default:
             cout << "Lua chon khong hop le! Vui long chon lai." << endl;
             break;
-    }
+        }  
+    }while(choice>0||choice<5);
 
     return choice;
 }
