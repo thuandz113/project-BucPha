d017da1323df13a5df8d7d9a8e243f1753e6745d
NTann17 <tantrinh209@gmail.com>
2024-11-17 22:28:23 +0700

    fix

--- function/ql.h
+++ function/ql.h
@@ -145,3 +145,313 @@ string getCurrentDate()
 
     return to_string(day) + "/" + to_string(month) + "/" + to_string(year);
 }
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <chrono>
+#include <thread>
+#include <iomanip>
+
+#include "doiTuong.h"
+#include "ham.h"
+//============================Account==========================//
+using namespace std;
+class SanPham {
+protected:
+    string ten;         // Tên sản phẩm
+    int soLuong;        // Số lượng sản phẩm
+    int gia;         // Giá sản phẩm
+
+public:
+    virtual string getLoai() const = 0; // Trả về loại sản phẩm
+    virtual void nhapThongTin() = 0; // Nhập thông tin sản phẩm
+    virtual void luuThongTinVaoFile(ofstream& file) = 0; // Lưu thông tin vào file
+
+    string getTen() const { return ten; } // Lấy tên sản phẩm
+    int getSoLuong() const { return soLuong; } // Lấy số lượng
+    void setSoLuong(int sl) { soLuong = sl; } // Cập nhật số lượng
+    int getGia() const { return gia; } // Lấy giá sản phẩm
+
+    virtual ~SanPham() {} // Destructor ảo
+    void setGia(int giaSanPham) {
+        gia = giaSanPham;
+    }
+    void setTen(const string& tenSanPham) {
+        ten = tenSanPham;
+    }
+};
+
+class ThucPham : public SanPham {
+private:
+    string hanSuDung;
+
+public:
+    string getLoai() const override { return "ThucPham"; }
+
+    void nhapThongTin() override {
+        cout << "Nhap ten thuc pham: ";
+        cin.ignore();
+        getline(cin,ten);
+        cout << "Nhap gia: ";
+        cinInt >> gia;
+        cout << "Nhap so luong: ";
+        cinInt >> soLuong;
+    }
+
+    void luuThongTinVaoFile(ofstream& file) override {
+        file << "ThucPham," << ten << "," << gia << "," << soLuong << endl;
+    }
+
+    void khoiTaoTuFile(const string& ten, double gia, int soLuong) {
+        this->ten = ten;
+        this->gia = gia;
+        this->soLuong = soLuong;
+    }
+};
+
+class DoUong : public SanPham {
+private:
+    bool coCon;
+
+public:
+    string getLoai() const override { return "DoUong"; }
+
+    void nhapThongTin() override {
+        cout << "Nhap ten do uong: ";
+        cin.ignore();
+        getline(cin,ten);
+        cout << "Nhap gia: ";
+        cinInt >> gia;
+        cout << "Nhap so luong: ";
+        cinInt >> soLuong;
+
+    }
+
+    void luuThongTinVaoFile(ofstream& file) override {
+        file << "DoUong," << ten << "," << gia << "," << soLuong << endl;
+    }
+
+    void khoiTaoTuFile(const string& ten, double gia, int soLuong) {
+        this->ten = ten;
+        this->gia = gia;
+        this->soLuong = soLuong;
+
+    }
+};
+
+class ThoiTrang : public SanPham {
+private:
+    string size;
+
+public:
+    string getLoai() const override { return "ThoiTrang"; }
+
+    void nhapThongTin() override {
+        cout << "Nhap ten thoi trang: ";
+        cin.ignore();
+        getline(cin,ten);
+        cout << "Nhap gia: ";
+        cinInt >> gia;
+        cout << "Nhap so luong: ";
+        cinInt >> soLuong;
+
+    }
+
+    void luuThongTinVaoFile(ofstream& file) override {
+        file << "ThoiTrang," << ten << "," << gia << "," << soLuong << endl;
+    }
+
+    void khoiTaoTuFile(const string& ten, double gia, int soLuong) {
+        this->ten = ten;
+        this->gia = gia;
+        this->soLuong = soLuong;
+
+    }
+};
+
+class DoGiaDung : public SanPham {
+private:
+    string chatLieu;
+
+public:
+    string getLoai() const override { return "DoGiaDung"; }
+
+    void nhapThongTin() override {
+        cout << "Nhap ten do gia dung: ";
+        cin.ignore();
+        getline(cin,ten);
+        cout << "Nhap gia: ";
+        cinInt >> gia;
+        cout << "Nhap so luong: ";
+        cinInt >> soLuong;
+
+    }
+
+    void luuThongTinVaoFile(ofstream& file) override {
+        file << "DoGiaDung," << ten << "," << gia << "," << soLuong << endl;
+    }
+
+    void khoiTaoTuFile(const string& ten, double gia, int soLuong) {
+        this->ten = ten;
+        this->gia = gia;
+        this->soLuong = soLuong;
+    }
+};
+
+class DoDienTu : public SanPham {
+private:
+    string hangSX;
+
+public:
+    string getLoai() const override { return "DoDienTu"; }
+
+    void nhapThongTin() override {
+        cout << "Nhap ten do dien tu: ";
+        cin.ignore();
+        getline(cin,ten);
+        cout << "Nhap gia: ";
+        cinInt >> gia;
+        cout << "Nhap so luong: ";
+        cinInt >> soLuong;
+
+    }
+
+    void luuThongTinVaoFile(ofstream& file) override {
+        file << "DoDienTu," << ten << "," << gia << "," << soLuong << endl;
+    }
+
+    void khoiTaoTuFile(const string& ten, double gia, int soLuong) {
+        this->ten = ten;
+        this->gia = gia;
+        this->soLuong = soLuong;
+    }
+};
+class QuanLyChucNang {
+private:
+    vector<SanPham*> danhSachSanPham;
+    
+public:
+
+    enum LoaiSanPham {
+        THUC_PHAM,//thuc pham
+        DO_UONG,
+        THOI_TRANG,
+        DO_GIA_DUNG,
+        DO_DIEN_TU
+    };
+
+    void chonQuayKhac(UserAccount &user) {
+
+        
+        cout << "=============================\n";
+        cout << "| 1. Thuc pham              |\n";
+        cout << "| 2. Do uong                |\n";
+        cout << "| 3. Thoi trang             |\n";
+        cout << "| 4. Do gia dung            |\n";
+        cout << "| 5. Do dien tu             |\n";
+        cout << "| 6. Thoat                  |\n";
+        cout << "=============================\n";
+        cout << " Chon loai san pham muon mua:\n";
+        int luaChonLoai;
+        cinInt >> luaChonLoai;
+
+        while (luaChonLoai < 1 || luaChonLoai > 6) {
+            cout << "Lua chon khong hop le. Vui long nhap tu 1 den 6: ";
+            cinInt >> luaChonLoai;
+        }
+
+        switch (luaChonLoai) {
+            case 1: {
+                this_thread::sleep_for(chrono::milliseconds(500));
+                system("cls");
+                muaSanPham(THUC_PHAM,user);
+                break;
+            }
+            case 2: {
+                this_thread::sleep_for(chrono::milliseconds(500));
+                system("cls");
+                muaSanPham(DO_UONG,user);
+                break;
+            }
+            case 3: {
+                this_thread::sleep_for(chrono::milliseconds(500));
+                system("cls");
+                muaSanPham(THOI_TRANG,user);
+                break;
+            }
+            case 4: {
+                this_thread::sleep_for(chrono::milliseconds(500));
+                system("cls");
+                muaSanPham(DO_GIA_DUNG,user);
+                break;
+            }   
+            case 5: {
+                this_thread::sleep_for(chrono::milliseconds(500));
+                system("cls");
+                muaSanPham(DO_DIEN_TU,user);
+                break;
+            }
+            case 6: break;
+        }
+    }
+
+    void thanhToan(vector<int>& luaChonSanPham, vector<int>& soLuongMua, UserAccount &user) {
+    double tongTien = 0.0;
+
+    // Tiêu đề hóa đơn
+    cout << "================================================================================\n";
+    cout<<  "|                                   HOA DON                                    |\n";
+    cout << "================================================================================\n";
+    cout << "| " << left << setw(30) << "Ten San Pham"                                       
+         << "| " << setw(10) << "So Luong" 
+         << "| " << setw(15) << "Gia (VND)" 
+         << "| " << setw(15) << "Thanh Tien (VND)" << "|\n";
+    cout << "================================================================================\n";
+
+    // Hiển thị danh sách sản phẩm đã mua
+    for (size_t i = 0; i < luaChonSanPham.size(); ++i) 
+    {
+        int index = luaChonSanPham[i];
+        double giaSanPham = danhSachSanPham[index]->getGia();
+        int soLuong = soLuongMua[i];
+        double thanhTien = giaSanPham * soLuong;
+        tongTien += thanhTien;
+
+        // In ra từng sản phẩm
+        cout << "| " << left << setw(30) << danhSachSanPham[index]->getTen() 
+             << "| " << setw(10) << soLuong 
+             << "| " << setw(15) << giaSanPham 
+             << "| " << setw(15) << thanhTien << " |\n";
+    }
+
+    cout << "================================================================================\n";
+    cout << "Tong so tien can thanh toan: " << tongTien << " VND" << endl;
+
+    char luaChon;
+    cout << "Ban co muon thanh toan khong? (y/n): ";
+    cin >> luaChon;
+    
+    if (luaChon == 'y' || luaChon == 'Y') 
+    {
+        if(user.getCredits() >= tongTien)
+        {
+            string fileName;
+            fileName = "database/history/" + user.getUsername() + ".txt";
+            ofstream fileHoaDon(fileName, ios::app);
+            if(!fileHoaDon.is_open())
+            {
+                cout<<"Khong the mo file hoa don!"<<endl;
+                return;
+            }
+            for (size_t i = 0; i < luaChonSanPham.size(); ++i) 
+            {
+                int index = luaChonSanPham[i];
+                double giaSanPham = danhSachSanPham[index]->getGia();
+                int soLuong = soLuongMua[i];
+                double thanhTien = giaSanPham * soLuong;
+                fileHoaDon<<danhSachSanPham[index]->getTen()<<" "<<giaSanPham<<" "<<soLuong<< " "<<thanhTien<< " "<< getCurrentDate()<<endl;
+            }
